80092fee19e27b3a19f5a0d06f46653ba056d0e2
Jiri Tomicek <j.tomicek@gmail.com>
2018-04-09 10:51:36 +0200

    test of access to repository

---
+++ check_scripts/vector_checks/fgdb2pg.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Import ESRI FGDB into (existing) PostGIS DB.
+"""
+
+__author__ = "Jiri Tomicek"
+__copyright__ = "Copyright 2018, GISAT s.r.o., CZ"
+__email__ = "jiri.tomicek@gisat.cz"
+__status__ = "testing"
