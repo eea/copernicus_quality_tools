208980105d63326398848afe6de2bf3b137102c5
Jiri Tomicek <j.tomicek@gmail.com>
2018-04-20 14:22:43 +0200

    igorova verze

---
+++ igor/registry.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+
+check_function_registry = {}
+
+def register_check_function(ident, description=None):
+    # If name is supplied with dots, take the last part.
+    ident = ident.split(".")[-1]
+
+    # Check if the function has already been registered.
+    if ident in check_function_registry:
+        raise Exception("An attemp to reregister function with ident='%s'.".format(ident))
+
+    # Return decorator.
+    def register(func):
+        func.ident = ident
+        func.description = description
+        check_function_registry[ident] = func
+        return func
+    return register
+
+def get_check_function(check_ident):
+    return check_function_registry[check_ident]
