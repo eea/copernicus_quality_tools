113339404debbaa7c523813b0fdd0136acb7a1b6
Jiri Tomicek <j.tomicek@gmail.com>
2018-04-25 15:06:17 +0200

    add v1 check and r1 check ready to use in the environment (file format check) update _check_defaults.json: split vr1 configuration data into v1 and r1...

---
+++ src/qc_tool/wps/vector_check/v1.py
@@ -0,0 +1,55 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+File format check.
+"""
+
+import os
+import ogr
+
+from registry import register_check_function
+
+@register_check_function(__name__, "File format is allowed.")
+def run_check(filepath, params):
+    """
+    File format check.
+    :param filepath: pathname to data source
+    :param params: configuration
+    :return: status + message
+    """
+
+    print("run_check.filepath={:s}".format(repr(filepath)))
+    print("run_check.params={:s}".format(repr(params)))
+
+    # enable ogr to use exceptions
+    ogr.UseExceptions()
+
+    # file extension check
+    ds_extension = os.path.splitext(filepath)[1]
+    if ds_extension not in params["format"]:
+        return {"status": "failed",
+                "message": "forbidden file extension"}
+
+    # try to open file with ogr drivers
+    if ds_extension in params["drivers"]:
+        try:
+            ds_open = ogr.Open(filepath)
+            if ds_open is None:
+                return {"status": "failed",
+                        "message": "file can not be opened"}
+        except:
+            return {"status": "failed",
+                    "message": "file can not be opened"}
+
+        # check file format
+        drivername = ds_open.GetDriver().GetName()
+        if drivername == params["drivers"][ds_extension]:
+            return {"status": "ok",
+                    "message": "the file format check was successful"}
+        else:
+            return {"status": "failed",
+                    "message": "file format is invalid"}
+    else:
+        return {"status": "failed",
+                "message": "forbidden file extension (%s)" % ds_extension}
