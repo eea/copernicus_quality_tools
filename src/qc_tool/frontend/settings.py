f3dc54b1c1b56bb16bf511009bceb62380c42b36
jirikadlec2 <jirikadlec2@gmail.com>
2018-04-30 18:14:19 +0200

    qc tool: use mounted volume for data files

--- src/qc_tool/frontend/settings.py
+++ src/qc_tool/frontend/settings.py
@@ -136,4 +136,5 @@ MEDIA_ROOT = os.path.join(os.path.basename(os.path.abspath(os.path.dirname('__FI
 
 dirname = os.path.dirname
 PRODUCT_TYPES_DIR = os.path.join(dirname(dirname(dirname(os.path.abspath('.')))), 'product_types')
-CHECKED_FILES_DIR = os.path.join(dirname(dirname(dirname(os.path.abspath('.')))), 'testing_data')
+#CHECKED_FILES_DIR = os.path.join(dirname(dirname(dirname(os.path.abspath('.')))), 'testing_data')
+CHECKED_FILES_DIR = "/mnt/freenas_pracovni_archiv_01/projects/cop15m/volume-wps/wps/data"
