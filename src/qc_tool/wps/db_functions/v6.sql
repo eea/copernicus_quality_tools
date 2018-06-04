334979c8381e06f8c8dc291f12f869a1b56e3200
jirikadlec2 <jirikadlec2@gmail.com>
2018-06-04 19:15:31 +0200

    updated v6.sql function from Erika: now includes the 'technical changes' condition

--- src/qc_tool/wps/db_functions/v6.sql
+++ src/qc_tool/wps/db_functions/v6.sql
@@ -1,4 +1,5 @@
 -- FUNCTION: __v6_validcodes(text, text)
+
 -- DROP FUNCTION __v6_validcodes(text, text);
 
 CREATE OR REPLACE FUNCTION __v6_validcodes(
@@ -9,7 +10,7 @@ CREATE OR REPLACE FUNCTION __v6_validcodes(
     LANGUAGE 'plpgsql'
 
     COST 100
-    VOLATILE
+    VOLATILE 
 AS $BODY$
 
 DECLARE
@@ -17,7 +18,8 @@ DECLARE
   code_from text;
   code_to text;
   code text;
-
+  Tcount integer :=0;
+	
 BEGIN
 	EXECUTE format('DROP TABLE IF EXISTS %1$s_ValidCodes_error', product_name);
 
@@ -25,54 +27,55 @@ BEGIN
 		when 'clc' then
 			CASE substring(product_name,4,2)
 				when '06' then code='code_06';
-					EXECUTE format('CREATE TABLE %1$s_ValidCodes_error AS SELECT * FROM %1$s WHERE %2$s NOT IN (SELECT code FROM clc_code)',product_name,code);
+					EXECUTE format('CREATE TABLE %1$s_ValidCodes_error AS SELECT * FROM %1$s WHERE %2$s NOT IN (SELECT code FROM V6_code WHERE data_type=''CLC'')',product_name,code);
 					EXECUTE 'SELECT count(*) FROM ' ||  product_name || '_ValidCodes_error' INTO _ct;
 				when '12' then code='code_12';
-					EXECUTE format('CREATE TABLE %1$s_ValidCodes_error AS SELECT * FROM %1$s WHERE %2$s NOT IN (SELECT code FROM clc_code)',product_name,code);
-					EXECUTE 'SELECT count(*) FROM ' ||  product_name || '_ValidCodes_error' INTO _ct;
+					EXECUTE format('CREATE TABLE %1$s_ValidCodes_error AS SELECT * FROM %1$s WHERE %2$s NOT IN (SELECT code FROM V6_code WHERE data_type=''CLC'')',product_name,code);
+					EXECUTE 'SELECT count(*) FROM ' ||  product_name || '_ValidCodes_error' INTO _ct;					
 				when '18' then code='code_18';
-					EXECUTE format('CREATE TABLE %1$s_ValidCodes_error AS SELECT * FROM %1$s WHERE %2$s NOT IN (SELECT code FROM clc_code)',product_name,code);
-					EXECUTE 'SELECT count(*) FROM ' ||  product_name || '_ValidCodes_error' INTO _ct;
-				else
+					EXECUTE format('CREATE TABLE %1$s_ValidCodes_error AS SELECT * FROM %1$s WHERE %2$s NOT IN (SELECT code FROM V6_code WHERE data_type=''CLC'')',product_name,code);
+					EXECUTE 'SELECT count(*) FROM ' ||  product_name || '_ValidCodes_error' INTO _ct;			
+				else 
 					SELECT 999999 INTO _ct;
 			END CASE;
 
-		when 'cha' then
+		when 'cha' then	
 			CASE substring(product_name,4,2)
 				when '06' then
 					code_from='code_00';
-					EXECUTE format('CREATE TABLE %1$s_ValidCodes_error AS SELECT * FROM %1$s WHERE %2$s NOT IN (SELECT code FROM clc_code)',product_name,code_from);
-					code_to='code_06';
-					EXECUTE format('INSERT INTO %1$s_ValidCodes_error SELECT * FROM %1$s WHERE %2$s NOT IN (SELECT code FROM clc_code)',product_name,code_to);
-					EXECUTE 'SELECT count(*) FROM ' ||  product_name || '_ValidCodes_error' INTO _ct;
+					EXECUTE format('CREATE TABLE %1$s_ValidCodes_error AS SELECT * FROM %1$s WHERE %2$s NOT IN (SELECT code FROM V6_code WHERE data_type=''CLC'')',product_name,code_from);		
+					code_to='code_06';				
+					EXECUTE format('INSERT INTO %1$s_ValidCodes_error SELECT * FROM %1$s WHERE %2$s NOT IN (SELECT code FROM V6_code WHERE data_type=''CLC'')',product_name,code_to);
+					EXECUTE 'SELECT count(*) FROM ' ||  product_name || '_ValidCodes_error' INTO _ct;	
 				when '12' then
 					code_from='code_06';
-					EXECUTE format('CREATE TABLE %1$s_ValidCodes_error AS SELECT * FROM %1$s WHERE %2$s NOT IN (SELECT code FROM clc_code)',product_name,code_from);
+					EXECUTE format('CREATE TABLE %1$s_ValidCodes_error AS SELECT * FROM %1$s WHERE %2$s NOT IN (SELECT code FROM V6_code WHERE data_type=''CLC'')',product_name,code_from);						
 					code_to='code_12';
-					EXECUTE format('INSERT INTO %1$s_ValidCodes_error SELECT * FROM %1$s WHERE %2$s NOT IN (SELECT code FROM clc_code)',product_name,code_to);
-					EXECUTE 'SELECT count(*) FROM ' ||  product_name || '_ValidCodes_error' INTO _ct;
+					EXECUTE format('INSERT INTO %1$s_ValidCodes_error SELECT * FROM %1$s WHERE %2$s NOT IN (SELECT code FROM V6_code WHERE data_type=''CLC'')',product_name,code_to);
+					EXECUTE format('INSERT INTO %1$s_ValidCodes_error SELECT * FROM %1$s WHERE %2$s=%3$s and chtype <>''T''',product_name,code_from,code_to);
+					EXECUTE 'SELECT count(*) FROM ' ||  product_name || '_ValidCodes_error' INTO _ct;	
 				when '18' then
 					code_from='code_12';
-					EXECUTE format('CREATE TABLE %1$s_ValidCodes_error AS SELECT * FROM %1$s WHERE %2$s NOT IN (SELECT code FROM clc_code)',product_name,code_from);
+					EXECUTE format('CREATE TABLE %1$s_ValidCodes_error AS SELECT * FROM %1$s WHERE %2$s NOT IN (SELECT code FROM V6_code WHERE data_type=''CLC'')',product_name,code_from);						
 					code_to='code_18';
-					EXECUTE format('INSERT INTO %1$s_ValidCodes_error SELECT * FROM %1$s WHERE %2$s NOT IN (SELECT code FROM clc_code)',product_name,code_to);
-					EXECUTE 'SELECT count(*) FROM ' ||  product_name || '_ValidCodes_error' INTO _ct;
-				else
+					EXECUTE format('INSERT INTO %1$s_ValidCodes_error SELECT * FROM %1$s WHERE %2$s NOT IN (SELECT code FROM V6_code WHERE data_type=''CLC'')',product_name,code_to);
+					EXECUTE 'SELECT count(*) FROM ' ||  product_name || '_ValidCodes_error' INTO _ct;	
+				else 
 					SELECT 999999 INTO _ct;
 			END CASE;
 
   		when 'ua' then
 			CASE right(product_name,6)
 				when 'ua2006' then code='code2006';
-					EXECUTE format('CREATE TABLE %1$s_ValidCodes_error AS SELECT * FROM %1$s WHERE %2$s NOT IN (SELECT code FROM ua_code)',product_name,code);
+					EXECUTE format('CREATE TABLE %1$s_ValidCodes_error AS SELECT * FROM %1$s WHERE %2$s NOT IN (SELECT code FROM V6_code WHERE data_type=''UA'')',product_name,code);
 					EXECUTE 'SELECT count(*) FROM ' ||  product_name || '_ValidCodes_error' INTO _ct;
 				when 'ua2012' then code='code2012';
-					EXECUTE format('CREATE TABLE %1$s_ValidCodes_error AS SELECT * FROM %1$s WHERE %2$s NOT IN (SELECT code FROM ua_code)',product_name,code);
-					EXECUTE 'SELECT count(*) FROM ' ||  product_name || '_ValidCodes_error' INTO _ct;
+					EXECUTE format('CREATE TABLE %1$s_ValidCodes_error AS SELECT * FROM %1$s WHERE %2$s NOT IN (SELECT code FROM V6_code WHERE data_type=''UA'')',product_name,code);
+					EXECUTE 'SELECT count(*) FROM ' ||  product_name || '_ValidCodes_error' INTO _ct;					
 				when 'ua2018' then code='code2018';
-					EXECUTE format('CREATE TABLE %1$s_ValidCodes_error AS SELECT * FROM %1$s WHERE %2$s NOT IN (SELECT code FROM ua_code)',product_name,code);
-					EXECUTE 'SELECT count(*) FROM ' ||  product_name || '_ValidCodes_error' INTO _ct;
-				else
+					EXECUTE format('CREATE TABLE %1$s_ValidCodes_error AS SELECT * FROM %1$s WHERE %2$s NOT IN (SELECT code FROM V6_code WHERE data_type=''UA'')',product_name,code);
+					EXECUTE 'SELECT count(*) FROM ' ||  product_name || '_ValidCodes_error' INTO _ct;			
+				else 
 					SELECT 999999 INTO _ct;
 			END CASE;
 
@@ -80,23 +83,23 @@ BEGIN
 			CASE right(product_name,16)
 				when 'change_2006_2012' then
 					code_from='code2006';
-					EXECUTE format('CREATE TABLE %1$s_ValidCodes_error AS SELECT * FROM %1$s WHERE %2$s NOT IN (SELECT code FROM ua_code)',product_name,code_from);
+					EXECUTE format('CREATE TABLE %1$s_ValidCodes_error AS SELECT * FROM %1$s WHERE %2$s NOT IN (SELECT code FROM V6_code WHERE data_type=''UA'')',product_name,code_from);						
 					code_to='code2012';
-					EXECUTE format('INSERT INTO %1$s_ValidCodes_error SELECT * FROM %1$s WHERE %2$s NOT IN (SELECT code FROM ua_code)',product_name,code_to);
-					EXECUTE 'SELECT count(*) FROM ' ||  product_name || '_ValidCodes_error' INTO _ct;
+					EXECUTE format('INSERT INTO %1$s_ValidCodes_error SELECT * FROM %1$s WHERE %2$s NOT IN (SELECT code FROM V6_code WHERE data_type=''UA'')',product_name,code_to);
+					EXECUTE 'SELECT count(*) FROM ' ||  product_name || '_ValidCodes_error' INTO _ct;	
 				when 'change_2012_2018' then
 					code_from='code2012';
-					EXECUTE format('CREATE TABLE %1$s_ValidCodes_error AS SELECT * FROM %1$s WHERE %2$s NOT IN (SELECT code FROM ua_code)',product_name,code_from);
+					EXECUTE format('CREATE TABLE %1$s_ValidCodes_error AS SELECT * FROM %1$s WHERE %2$s NOT IN (SELECT code FROM V6_code WHERE data_type=''UA'')',product_name,code_from);						
 					code_to='code2018';
-					EXECUTE format('INSERT INTO %1$s_ValidCodes_error SELECT * FROM %1$s WHERE %2$s NOT IN (SELECT code FROM ua_code)',product_name,code_to);
-					EXECUTE 'SELECT count(*) FROM ' ||  product_name || '_ValidCodes_error' INTO _ct;
-				else
+					EXECUTE format('INSERT INTO %1$s_ValidCodes_error SELECT * FROM %1$s WHERE %2$s NOT IN (SELECT code FROM V6_code WHERE data_type=''UA'')',product_name,code_to);
+					EXECUTE 'SELECT count(*) FROM ' ||  product_name || '_ValidCodes_error' INTO _ct;	
+				else 
 					SELECT 999999 INTO _ct;
 			END CASE;
-		else
-			SELECT 999999 INTO _ct;
-	END CASE;
+		else 
+			SELECT 999999 INTO _ct;	
+	END CASE;	
 
-END
+END  
 
 $BODY$;
