8d935b0f79868658bb4440993c8d0d35fd8f3b9e
jirikadlec2 <jirikadlec2@gmail.com>
2018-06-07 20:50:52 +0200

    test for reporting status from WPS dispatch

--- src/qc_tool/test/test_product_type.py
+++ src/qc_tool/test/test_product_type.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import json
 
 from unittest import TestCase
 from uuid import uuid4
@@ -28,5 +29,20 @@ class Test_clc_YY(TestCase):
         filepath = TEST_DATA_DIR.joinpath("clc2012_mt.gdb")
         result = dispatch(str(uuid4()), filepath, self.product_type_name, [])
         self.assertEqual("ok", result["r1"]["status"], "Malta should pass the checks for clc_YY product type")
+
+
+class Test_update_status(TestCase):
+    product_type_name = "clc_YY"
+
+    def test_run(self):
+
+        def my_update(suite_result):
+            print("-------UPDATE STATUS ------")
+            print(suite_result)
+
+        filepath = TEST_DATA_DIR.joinpath("clc2012_mt.gdb")
+        result = dispatch(str(uuid4()), filepath, self.product_type_name,
+                          ["v1", "v2", "v3", "v4", "v5", "v6", "v8", "v11"], update_result_func=my_update)
+        #self.assertEqual("ok", result["v1"]["status"], "clc2012_CZ should pass the checks for clc_YY product type")
 
 
