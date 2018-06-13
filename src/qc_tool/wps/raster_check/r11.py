fa574a26ced1bbdbbafe2ee3205fd1e260954217
jirikadlec2 <jirikadlec2@gmail.com>
2018-06-13 15:30:05 +0200

    updated r11 raster MMU check. works with GRASS in the qc_tool_wps docker.

--- src/qc_tool/wps/raster_check/r11.py
+++ src/qc_tool/wps/raster_check/r11.py
@@ -5,133 +5,121 @@
 Minimum mapping unit check.
 """
 
-import subprocess
-import sys
-import os
 from pathlib import Path
-import ogr
-from math import ceil
+from subprocess import run
+
+from osgeo import ogr
 
 from qc_tool.wps.registry import register_check_function
-
-
-def setup_grass_scripting(grass_version):
-    """
-    setup calling grass functions (for LINUX only!!!)
-    :param grass_version: [string]; e.g.: 'grass72'
-    :return: path to GISBASE environment variable
-    """
-
-    # find GRASS GIS start script path
-    startcmd = grass_version + ' --config path'
-    try:
-        p = subprocess.Popen(startcmd, shell=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        out, err = p.communicate()
-    except OSError as error:
-        return {"status": "failed",
-                "message": "Cannot find GRASS GIS start script {cmd}: {error}".format(cmd=startcmd[0], error=error)}
-
-    # if the error occured, print message and exit
-    if p.returncode != 0:
-        return {"status": "failed",
-                "message": "Issues running GRASS GIS start script"" {cmd}: {error}".format(cmd=' '.join(startcmd),
-                                                                                           error=err)}
-
-    # Set GISBASE environment variable
-    gisbase = str(out.strip(b'\n'))
-    os.environ['GISBASE'] = gisbase
-
-    # define GRASS-Python environment
-    gpydir = Path(gisbase, "etc", "python")
-    sys.path.append(str(gpydir))
-    return gisbase
-
-
-def start_grass(gdbdir, georef_data, version):
-
-    """
-    Initialize GRASS GIS session.
-    :param georef_data: georeferenced shapefile or georaster
-    :param version: [string]: version of GRASS GIS e.g. 'grass'
-    :return:
-    """
-    print(sys.path)
-
-    # create temporary GRASS database directory, location
-    GrassDbDir = gdbdir
-    location = 'location' # name of location
-
-    # set up grass for scripting...
-    gisbase = setup_grass_scripting(version)
-
-    # Import GRASS Python bindings
-    import grass.script as g
-    import grass.script.setup as gsetup
-    from grass.script import core as grass_core
-
-    # Launch GRASS session
-    gsetup.init(gisbase, GrassDbDir)
-
-    # set up CRS using georeferenced data
-    grass_core.create_location(GrassDbDir, location, filename=georef_data)
-
-    # launch GRASS session, location, mapset...
-    gsetup.init(gisbase, GrassDbDir, location, 'PERMANENT')
-    return g
 
 
 @register_check_function(__name__, "Minimum mapping unit check.")
 def run_check(filepath, params):
     """
-    CRS check.
+    Minimum mapping unit (MMU) check.
     :param filepath: pathname to data source
     :param params: configuration
     :return: status + message
     """
 
     # convert mmu limit from 'ha' to 'pix' (expected pixel size of 20m)
-    mmu_limit = int(ceil(params["area_ha"]*25))
+    # mmu_limit = int(ceil(params["area_ha"]*25))
+    # for example 20x20 pixel and 5ha mmu -> 125 pixels mmu_limit
 
-    clump_centroids = Path(params["job_dir"], "clump_centroids.shp")
+    job_dir = params["job_dir"]
 
-    # TODO: create GRASS db dir in temporary dir from params
-    # start GRASS session
-    grass_db_dir = Path(params["job_dir"], 'grass') # create GRASS database directory
-    print(grass_db_dir)
-    grass_db_dir.mkdir()
+    lessmmu_shp_path = Path(job_dir, "lessmmu_areas.shp")
 
-    # start GRASS GIS session...
-    grass = start_grass(bytes(str(grass_db_dir), encoding='utf-8'), filepath, "grass")
+    GRASS_VERSION = "grass72"
 
-    # import source raster data into GRASS environment
-    grass.read_command('r.in.gdal', input=filepath, output='inpfile')
+    # (1) create a new GRASS location named "location" in the job directory
+    location_path = Path(job_dir, "location")
+    p1 = run([GRASS_VERSION,
+             "-c",
+             filepath,
+             "-e",
+             str(location_path)], check=True)
+    if p1.returncode != 0:
+        return {"status": "failed", "message": "GRASS GIS error: cannot create location!"}
 
-    # build clumps with unique codes from source data
-    grass.read_command('r.clump', 'd', input='inpfile', output='clumps', title='clumps')
+    # (2) import the data into a GRASS mapset named PERMANENT
+    mapset_path = Path(location_path, "PERMANENT")
+    p2 = run(["grass72",
+              str(mapset_path),
+              "--exec",
+              "r.in.gdal",
+              "input={:s}".format(filepath),
+              "output=inpfile"],
+             check=True)
+    if p2.returncode != 0:
+        return {"status": "failed", "message": "GRASS GIS error: cannot import raster from {:s}".format(filepath)}
 
-    # create vector layer of centroids of clumps with statistics
-    grass.read_command('r.volume', overwrite=True, input='clumps', clump='clumps', centroids='clump_centroids', output='clump_stats')
+    # (3) run r.reclass.area (area is already in hectares)
+    mmu_limit_ha = params["area_ha"]
+    p3 = run([GRASS_VERSION,
+              str(mapset_path),
+              "--exec",
+              "r.reclass.area",
+              "--verbose",
+              "input=inpfile",
+              "output=lessmmu_raster",
+              "value={:d}".format(mmu_limit_ha),
+              "mode=lesser",
+              "method=reclass"],
+             check=True)
+    if p3.returncode != 0:
+        return {"status": "failed", "message": "GRASS GIS error in r.reclass.area"}
 
-    # attribute based selection of less-mmu clumps
-    grass.read_command('v.extract', input='clump_centroids', output='lessmmu_centroids', where='count < {:s}'.format(str(mmu_limit)))
+    # (4) run r.to.vect: convert clumps with mmu < mmu_limit_ha to polygon areas
+    p4 = run([GRASS_VERSION,
+              str(mapset_path),
+              "--exec",
+              "r.to.vect",
+              "--overwrite",
+              "--verbose",
+              "-s",
+              "-v",
+              "type=area",
+              "input=lessmmu_raster",
+              "output=lessmmu_areas"],
+             check=True)
+    if p4.returncode != 0:
+        return {"status": "failed", "message": "GRASS GIS error in r.to.vect"}
 
-    # remove redundant fields from attr. table (some attributes contains too large numbers leading to errors in export centroids into shapefile)
-    grass.read_command('v.db.dropcolumn', map='lessmmu_centroids', columns='volume, average, sum')
+    # (5) export lessmmu_areas to shapefile
+    p5 = run([GRASS_VERSION,
+              str(mapset_path),
+              "--exec",
+              "v.out.ogr",
+              "input=lessmmu_areas",
+              "output={:s}".format(str(lessmmu_shp_path)),
+              "format=ESRI_Shapefile"],
+             check=True)
+    if p5.returncode != 0:
+        return {"status": "failed", "message": "GRASS GIS error in executing v.out.ogr"}
 
-    # export centroids of less-mmu clumps into shapefile
-    grass.read_command('v.out.ogr', 'c', input='lessmmu_centroids', output=clump_centroids, format='ESRI_Shapefile',
-                       output_layer='clump_centroids')
+    if not lessmmu_shp_path.exists():
+        return {"status": "failed",
+                "message": "GRASS GIS error. exported lessmmu_areas shapefile {:s} does not exist.".format(str(lessmmu_shp_path))}
 
-    # get lessmmu clumps count
-    ds_centr = ogr.Open(str(clump_centroids))
-    lyr_centr = ds_centr.GetLayer()
-    lessmmu_count = lyr_centr.GetFeatureCount()
-    lyr_centr = None
-    ds_centr = None
+    ogr.UseExceptions()
+    try:
+        ds_lessmmu = ogr.Open(str(lessmmu_shp_path))
+        if ds_lessmmu is None:
+            return {"status": "failed",
+                    "message": "GRASS GIS error or OGR error. The file {:s} can not be opened.".format(str(lessmmu_shp_path))}
+    except BaseException as e:
+        return {"status": "failed",
+                "message": "GRASS GIS error or OGR error. The shapefile {:s} cannot be opened. Exception: {:s}".format(
+                    str(lessmmu_shp_path, str(e)))}
+
+    lyr = ds_lessmmu.GetLayer()
+    lessmmu_count = lyr.GetFeatureCount()
 
     if lessmmu_count == 0:
-        return {"status": "ok"}
+        result = {"status": "ok"}
     else:
-        return {"status": "failed",
+        result = {"status": "failed",
                 "message": "The data source has {:s} objects under MMU limit of {:s} ha.".format(str(lessmmu_count),
                                                                                                  str(params["area_ha"]))}
+    return result
