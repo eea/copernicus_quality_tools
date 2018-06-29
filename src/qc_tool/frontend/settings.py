defcbdc0f14844e99cb745df931a00764d2541e7
jirikadlec2 <jirikadlec2@gmail.com>
2018-06-29 17:19:19 +0200

    Introduced user login and logout in the UI.
    The user name of the current user is shown in the page header.

--- src/qc_tool/frontend/settings.py
+++ src/qc_tool/frontend/settings.py
@@ -110,6 +110,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# for development do not use complex validators
+AUTH_PASSWORD_VALIDATORS = []
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
