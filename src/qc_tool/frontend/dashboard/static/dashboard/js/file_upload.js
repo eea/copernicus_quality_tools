7491d516dc9893a2be79e2553240fd6d31ac19ee
jirikadlec2 <jirikadlec2@gmail.com>
2018-07-10 00:43:05 +0200

    File upload progress reporting: show number of completed files.

--- src/qc_tool/frontend/dashboard/static/dashboard/js/file_upload.js
+++ src/qc_tool/frontend/dashboard/static/dashboard/js/file_upload.js
@@ -1,4 +1,8 @@
 $(function () {
+
+  var n_selected = 0;
+  var n_uploaded = 0;
+
   /* 1. OPEN THE FILE EXPLORER WINDOW */
   $(".js-upload-files").click(function () {
     $("#fileupload").click();
@@ -11,23 +15,28 @@ $(function () {
     progressServerDecayExp: 2,
 
     sequentialUploads: true,  /* 1. SEND THE FILES ONE BY ONE */
+
     start: function (e) {  /* 2. WHEN THE UPLOADING PROCESS STARTS, SHOW THE MODAL */
       $("#modal-progress").modal("show");
-      //$(".progress-bar")
     },
     stop: function (e) {  /* 3. WHEN THE UPLOADING PROCESS FINALIZE, HIDE THE MODAL */
       $("#modal-progress").modal("hide");
     },
+
+    submit: function (e, data) {
+        n_selected += data.files.length;
+    },
+
     progressall: function (e, data) {  /* 4. UPDATE THE PROGRESS BAR */
-      console.log(data);
       var progress = parseInt(data.loaded / data.total * 100, 10);
       var strProgress = progress + "%";
       $(".progress-bar").css({"width": strProgress});
       $(".progress-bar").text(strProgress) ;
+      $(".modal-title").text("Uploading file " + n_uploaded + " / " + n_selected);
     },
     done: function (e, data) {  /* 3. PROCESS THE RESPONSE FROM THE SERVER */
+      n_uploaded += 1;
       if (data.result.is_valid) {
-        $("#files_heading").prepend("Uploaded file");
         $("#files_table tbody").prepend(
 
           '<tr><td><div class="alert alert-success">File <strong>' + data.result.url + '</strong> uploaded successfully.</div></td></tr>'
