8abcdf051544050d0968adbeaa7271c675d34fc0
Igor Kratochvíl <igor.kratochvil@gisat.cz>
2018-07-13 20:13:27 +0200

    Rewritten v6 check function.
    The python code does not need separate sql script anymore.
    Reparametrized product definitions.

--- src/qc_tool/wps/vector_check/v6.py
+++ src/qc_tool/wps/vector_check/v6.py
@@ -2,23 +2,35 @@
 # -*- coding: utf-8 -*-
 
 
+import re
+
 from qc_tool.common import FAILED_ITEMS_LIMIT
 from qc_tool.wps.helper import shorten_failed_items_message
 from qc_tool.wps.registry import register_check_function
+
+
+SQL = "CREATE TABLE {:s} AS SELECT {:s} FROM {:s} WHERE {:s} NOT IN (SELECT code FROM v6_code WHERE data_type=%s);"
 
 
 @register_check_function(__name__)
 def run_check(params, status):
     cursor = params["connection_manager"].get_connection().cursor()
     for layer_name in params["db_layer_names"]:
-        error_table_name = "{:s}_validcodes_error".format(layer_name)
-        cursor.execute("SELECT __V6_ValidCodes(%s, %s);", (layer_name, params["product_code"]))
-        cursor.execute("SELECT DISTINCT {0:s} FROM {1:s} ORDER BY {0:s};".format(params["ident_colname"], error_table_name))
-        if cursor.rowcount == 0:
-            cursor.execute("DROP TABLE {:s};".format(error_table_name))
+        if "code_regex" in params:
+            mobj = re.search(params["code_regex"], layer_name)
+            code = mobj.group(1)
+            column_defs = params["code_to_column_def"][code]
         else:
-            failed_ids = [row[0] for row in cursor.fetchmany(FAILED_ITEMS_LIMIT)]
-            failed_ids_message = shorten_failed_items_message(failed_ids, cursor.rowcount)
-            failed_message = "The layer {:s} has invalid code in rows: {:s}.".format(layer_name, failed_ids_message)
-            status.add_message(failed_message)
-            status.add_error_table(error_table_name)
+            column_defs = params["column_defs"]
+
+        for column_name, value_set_name in column_defs:
+            error_table_name = "{:s}_{:s}_validcodes_error".format(layer_name, column_name)
+            sql = SQL.format(error_table_name, params["ident_colname"], layer_name, column_name)
+            cursor.execute(sql, (value_set_name,))
+            if cursor.rowcount == 0:
+                cursor.execute("DROP TABLE {:s};".format(error_table_name))
+            else:
+                failed_ids_message = get_failed_ids_message(cursor, error_table_name, params["ident_colname"])
+                failed_message = "The layer {:s} has column {:s} with invalid codes in rows: {:s}.".format(layer_name, column_name, failed_ids_message)
+                status.add_message(failed_message)
+                status.add_error_table(error_table_name)
