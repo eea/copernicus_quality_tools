7cb6e5628753a8d8134f9385b5b73cbf767c4348
jirikadlec2 <jirikadlec2@gmail.com>
2018-09-14 17:21:04 +0200

    Modified parsing of EPSG authority in v4 projection check. Also added a fallback option when EPSG code is not explicitly specified in the checked dataset.

--- src/qc_tool/wps/vector_check/v4.py
+++ src/qc_tool/wps/vector_check/v4.py
@@ -13,22 +13,39 @@ def run_check(params, status):
     for layer_name, layer_filepath in params["layer_sources"]:
         ds = ogr.Open(str(layer_filepath))
         layer = ds.GetLayerByName(layer_name)
-        srs = osr.SpatialReference(layer.GetSpatialRef().ExportToWkt())
-        if not srs.IsProjected:
+        srs = layer.GetSpatialRef()
+        if srs is None:
             status.aborted()
-            status.add_message("Layer {:s} has no projection.".format(layer_name))
+            status.add_message("Layer {:s} has missing spatial reference system.".format(layer_name))
+            return
+
+        # Search EPSG authority code
+        authority_name = srs.GetAuthorityName(None)
+        authority_code = srs.GetAuthorityCode(None)
+
+        if authority_name == "EPSG" and authority_code is not None:
+            # compare EPSG code using the root-level EPSG authority
+            if authority_code not in map(str, params["epsg"]):
+                status.aborted()
+                status.add_message("Layer {:s} has illegal EPSG code {:s}.".format(layer_name, str(authority_code)))
+                return
         else:
-            epsg = srs.GetAttrValue("AUTHORITY", 1)
+            # If the EPSG code is not detected, try to compare if the actual and expected SRS instances represent
+            # the same spatial reference system.
+            srs_match = False
+            allowed_codes = params["epsg"]
+            for allowed_code in allowed_codes:
+                expected_srs = osr.SpatialReference()
+                expected_srs.ImportFromEPSG(allowed_code)
+                if srs.IsSame(expected_srs):
+                    srs_match = True
+                    break
 
-            # special case for ETRS_1989_LAEA: epsg code should be 3035
-            if epsg is None:
-                projcs = srs.GetAttrValue("PROJCS")
-                if projcs == "ETRS_1989_LAEA":
-                    epsg = 3035
-
-            if epsg is None:
+            if not srs_match:
                 status.aborted()
-                status.add_message("Layer {:s} has missing EPSG authority.".format(layer_name))
-            elif epsg not in map(str, params["epsg"]):
-                status.aborted()
-                status.add_message("Layer {:s} has illegal EPSG code {:s}.".format(layer_name, str(epsg)))
+                status.add_message("The SRS of Layer {:s} is not in the list of allowed spatial reference systems. "
+                                   "detected SRS: {:s}, list of allowed SRS's: {:s} ".format(
+                    layer_name,
+                    srs.ExportToWkt(),
+                    ", ".join(map(str, params["epsg"]))
+                ))
