28caad5f0ba4cd298b6bd0230cbc61dcc491f0dc
jirikadlec2 <jirikadlec2@gmail.com>
2018-09-18 11:08:33 +0200

    Fixed issue with admin user not being created in default installation.

--- src/qc_tool/frontend/dashboard/management/commands/create_default_user.py
+++ src/qc_tool/frontend/dashboard/management/commands/create_default_user.py
@@ -14,14 +14,37 @@ class Command(BaseCommand):
             '--password', dest='password', required=True,
             help='the password of the new user',
         )
+        parser.add_argument(
+            '--admin', dest='admin', required=False,
+            help='YES if the user should be admin, NO otherwise.'
+        )
 
     def handle(self, *args, **options):
         username = options['username']
         password = options['password']
+
+        if "admin" in options:
+            is_admin = options["admin"]
+        else:
+            is_admin = "NO"
+
         print("username: {:s}".format(username))
         print("password: {:s}".format(password))
+        print("is_admin: {:s}".format(is_admin))
+
         # create the user
-        user = User.objects.create_user(username=username,
-                                        email="{:s}@{:s}.com".format(username, username),
-                                        password=password)
-        print("Default user {:s} created successfully.".format(username))
+        if User.objects.filter(username=username).exists():
+            print("User not created. The user with username {:s} already exists.")
+            return
+
+        if is_admin == "YES":
+            # Creating a superuser (admin YES)
+            User.objects.create_superuser(username=username,
+                                            email="{:s}@{:s}.com".format(username, username),
+                                            password=password)
+        else:
+            # Creating a regular user (admin NO)
+            User.objects.create_user(username=username,
+                                            email="{:s}@{:s}.com".format(username, username),
+                                            password=password)
+            print("Default user {:s} created successfully.".format(username))
