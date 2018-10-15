48ca00483f96fb2bbe2d000e33967a117b752fb6
Igor Kratochvíl <igor.kratochvil@gisat.cz>
2018-10-15 17:07:09 +0200

    Restructuralized product definition of ua.
    Checks taking the same parameters have been collected.

--- src/qc_tool/test/test_product.py
+++ src/qc_tool/test/test_product.py
@@ -17,7 +17,7 @@ class Test_fty_YYYY_020m(TestCase):
         super().setUp()
         load_all_check_functions()
 
-    def test_run(self):
+    def test(self):
         filepath = TEST_DATA_DIR.joinpath("raster", "fty", "fty_2015_020m_si_03035_d04_test.tif.zip")
         job_status = dispatch(str(uuid4()), "user_name", filepath, "fty_YYYY_020m", ["r2", "r3", "r4", "r5"])
         self.assertEqual("r1", job_status["checks"][1]["check_ident"])
@@ -87,33 +87,25 @@ class Test_ua_shp(ProductTestCase):
         # The new test zip should pass all checks with ok status.
         self.filepath = TEST_DATA_DIR.joinpath("vector", "ua_shp", "EE003L0_NARVA.shp.zip")
 
-    def test_run(self):
+    def test(self):
         expected_check_statuses = {"v_unzip": "ok",
                                    "v1_ua": "ok",
-                                   "boundary.v2": "ok",
-                                   "boundary.v3": "ok",
-                                   "boundary.v4": "ok",
-                                   "boundary.v_import2pg": "ok",
-                                   "reference.v2": "ok",
+                                   "v2": "ok",
                                    "reference.v3": "ok",
-                                   "reference.v4": "ok",
-                                   "reference.v_import2pg": "ok",
-                                   "reference.v5": "ok",
-                                   "reference.v6": "ok",
-                                   "reference.v8": "ok",
-                                   "reference.v11_ua": "failed", #FIXME: the status should be ok in v11_ua.
-                                   "reference.v13": "ok",
-                                   "reference.v14": "ok"}
+                                   "boundary.v3": "ok",
+                                   "v4": "ok",
+                                   "v_import2pg": "ok",
+                                   "v5": "ok",
+                                   "v6": "ok",
+                                   "v8": "ok",
+                                   "v11_ua": "failed",
+                                   "v13": "ok",
+                                   "v14": "ok"}
         job_status = dispatch(str(uuid4()),
                               "user_name",
                               self.filepath,
                               "ua",
-                              ["reference.v5",
-                               "reference.v6",
-                               "reference.v8",
-                               "reference.v11_ua",
-                               "reference.v13",
-                               "reference.v14"])
+                              ["v5", "v6", "v8", "v11_ua", "v13", "v14"])
         check_statuses = dict((check_status["check_ident"], check_status["status"])
                               for check_status in job_status["checks"])
         self.assertDictEqual(expected_check_statuses, check_statuses)
@@ -123,111 +115,80 @@ class Test_ua_gdb(ProductTestCase):
     def setUp(self):
         super().setUp()
         load_all_check_functions()
+
+    def test_klagenfurt(self):
         # FIXME:
         # Klagenfurt test zip should be removed while it fails in some checks.
         # The new test zip should pass all checks with ok status.
-        self.filepath = TEST_DATA_DIR.joinpath("vector", "ua_gdb", "AT006L1_KLAGENFURT.zip")
-
-    def test_run_ok(self):
+        filepath = TEST_DATA_DIR.joinpath("vector", "ua_gdb", "AT006L1_KLAGENFURT.zip")
         expected_check_statuses = {"v_unzip": "ok",
                                    "v1_ua": "ok",
-                                   "boundary.v2": "ok",
-                                   "boundary.v3": "ok",
-                                   "boundary.v4": "ok",
-                                   "boundary.v_import2pg": "ok",
-                                   "reference.v2": "ok",
+                                   "v2": "ok",
                                    "reference.v3": "ok",
-                                   "reference.v4": "ok",
-                                   "reference.v_import2pg": "ok",
-                                   "reference.v5": "ok",
-                                   "reference.v6": "ok",
-                                   "reference.v8": "ok",
-                                   "reference.v11_ua": "failed",
-                                   "reference.v13": "failed",
-                                   "reference.v14": "ok"}
+                                   "boundary.v3": "ok",
+                                   "v4": "ok",
+                                   "v_import2pg": "ok",
+                                   "v5": "ok",
+                                   "v6": "ok",
+                                   "v8": "ok",
+                                   "v11_ua": "failed",
+                                   "v13": "failed",
+                                   "v14": "ok"}
         job_status = dispatch(str(uuid4()),
                               "user_name",
-                              self.filepath,
+                              filepath,
                               "ua",
-                              ["reference.v5", "reference.v6", "reference.v8", "reference.v11_ua", "reference.v13", "reference.v14"])
+                              ["v5", "v6", "v8", "v11_ua", "v13", "v14"])
         check_statuses = dict((check_status["check_ident"], check_status["status"])
                               for check_status in job_status["checks"])
         self.assertDictEqual(expected_check_statuses, check_statuses)
 
-    def test_kobenhavn_ok(self):
+    def test_kobenhavn(self):
         self.maxDiff = None
-        kobenhavn_filepath = TEST_DATA_DIR.joinpath("vector", "ua_gdb", "DK001L2_KOBENHAVN_clip.zip")
+        filepath = TEST_DATA_DIR.joinpath("vector", "ua_gdb", "DK001L2_KOBENHAVN_clip.zip")
         expected_check_statuses = {"v_unzip": "ok",
                                    "v1_ua": "ok",
-                                   "boundary.v2": "ok",
-                                   "boundary.v3": "ok",
-                                   "boundary.v4": "ok",
-                                   "boundary.v_import2pg": "ok",
-                                   "reference.v2": "ok",
+                                   "v2": "ok",
                                    "reference.v3": "ok",
-                                   "reference.v4": "ok",
-                                   "reference.v_import2pg": "ok",
-                                   "reference.v5": "ok",
-                                   "reference.v6": "ok",
-                                   "reference.v8": "ok",
-                                   "reference.v11_ua": "ok",
-                                   "reference.v13": "ok",
-                                   "reference.v14": "ok",
-                                   "combined.v2": "ok",
+                                   "boundary.v3": "ok",
                                    "combined.v3": "ok",
-                                   "combined.v4": "ok",
-                                   "combined.v_import2pg": "ok",
-                                   "combined.v5": "ok",
-                                   "combined.v6": "ok",
-                                   "combined.v8": "ok",
-                                   "combined.v11_ua": "ok",
-                                   "combined.v13": "ok",
-                                   "combined.v14": "ok",
-                                   "revised.v2": "ok",
                                    "revised.v3": "ok",
-                                   "revised.v4": "ok",
-                                   "revised.v_import2pg": "ok",
-                                   "revised.v6": "ok",
-                                   "revised.v8": "ok",
-                                   "revised.v11_ua": "ok",
-                                   "revised.v13": "ok",
-                                   "revised.v14": "ok",
-                                   "change.v2": "ok",
                                    "change.v3": "ok",
-                                   "change.v4": "ok",
-                                   "change.v_import2pg": "ok",
-                                   "change.v5": "ok",
+                                   "v4": "ok",
+                                   "v_import2pg": "ok",
+                                   "v5": "ok",
+                                   "reference.v6": "ok",
+                                   "combined.v6": "ok",
+                                   "revised.v6": "ok",
                                    "change.v6": "ok",
-                                   "change.v8": "ok",
+                                   "v8": "ok",
+                                   "reference.v11_ua": "ok",
+                                   "combined.v11_ua": "ok",
+                                   "revised.v11_ua": "ok",
                                    "change.v11_ua": "ok",
-                                   "change.v13": "ok",
+                                   "v13": "ok",
+                                   "reference.v14": "ok",
+                                   "combined.v14": "ok",
+                                   "revised.v14": "ok",
                                    "change.v14": "ok"}
         job_status = dispatch(str(uuid4()),
                               "user_name",
-                              kobenhavn_filepath,
+                              filepath,
                               "ua_with_change",
-                              ["reference.v5",
+                              ["v5",
                                "reference.v6",
-                               "reference.v8",
-                               "reference.v11_ua",
-                               "reference.v13",
-                               "reference.v14",
-                               "combined.v5",
                                "combined.v6",
-                               "combined.v8",
-                               "combined.v11_ua",
-                               "combined.v13",
-                               "combined.v14",
                                "revised.v6",
-                               "revised.v8",
-                               "revised.v11_ua",
-                               "revised.v13",
-                               "revised.v14",
-                               "change.v5",
                                "change.v6",
-                               "change.v8",
+                               "v8",
+                               "reference.v11_ua",
+                               "combined.v11_ua",
+                               "revised.v11_ua",
                                "change.v11_ua",
-                               "change.v13",
+                               "v13",
+                               "reference.v14",
+                               "combined.v14",
+                               "revised.v14",
                                "change.v14"])
         check_statuses = dict((check_status["check_ident"], check_status["status"])
                               for check_status in job_status["checks"])
