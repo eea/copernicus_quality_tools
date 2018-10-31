4e61dc6f17b770046e53955a517613dc8aa93b55
Igor Kratochvíl <igor.kratochvil@gisat.cz>
2018-10-31 18:21:39 +0100

    Corrected clc product definition.
    Added missing attributes "shape_area" and "shape_length".
    v14 utilizes primary attributes "code_06" and "code_12" instead of derived attribute change.

--- src/qc_tool/test/test_common.py
+++ src/qc_tool/test/test_common.py
@@ -10,7 +10,7 @@ from qc_tool.common import CONFIG
 class TestCommon(TestCase):
     def test_load_product_definition(self):
         from qc_tool.common import load_product_definition
-        product_definition = load_product_definition("clc")
+        product_definition = load_product_definition("clc_2012")
         self.assertIn("checks", product_definition)
         self.assertLess(1, len(product_definition["checks"]))
         self.assertEqual({"check_ident": "v2",
@@ -22,14 +22,14 @@ class TestCommon(TestCase):
     def test_get_product_descriptions(self):
         from qc_tool.common import get_product_descriptions
         product_descriptions = get_product_descriptions()
-        self.assertIn("clc", product_descriptions)
-        self.assertEqual("CORINE Land Cover", product_descriptions["clc"])
+        self.assertIn("clc_2012", product_descriptions)
+        self.assertEqual("CORINE Land Cover 2012", product_descriptions["clc_2012"])
 
     def test_prepare_empty_job_status(self):
         from qc_tool.common import prepare_empty_job_status
-        status = prepare_empty_job_status("clc")
-        self.assertEqual("clc", status["product_ident"])
-        self.assertEqual("CORINE Land Cover", status["description"])
+        status = prepare_empty_job_status("clc_2012")
+        self.assertEqual("clc_2012", status["product_ident"])
+        self.assertEqual("CORINE Land Cover 2012", status["description"])
         self.assertLess(4, len(status["checks"]))
         self.assertEqual("v2", status["checks"][2]["check_ident"])
         self.assertEqual("File format is correct.", status["checks"][2]["description"])
@@ -37,8 +37,8 @@ class TestCommon(TestCase):
         self.assertFalse(status["checks"][1]["system"])
         self.assertIsNone(status["checks"][1]["status"])
         self.assertIsNone(status["checks"][1]["messages"])
-        self.assertEqual("v_import2pg", status["checks"][6]["check_ident"])
-        self.assertTrue(status["checks"][6]["system"])
+        self.assertEqual("v_import2pg", status["checks"][7]["check_ident"])
+        self.assertTrue(status["checks"][7]["system"])
 
 class TestCommonWithConfig(TestCase):
     def setUp(self):
