6be892a187d2f3bd03a0a2627b005b961bb5f474
Igor Kratochvíl <igor.kratochvil@gisat.cz>
2018-11-12 15:00:32 +0100

    Removed already unused dir_recursive_search().

--- src/qc_tool/wps/helper.py
+++ src/qc_tool/wps/helper.py
@@ -10,21 +10,6 @@ import zipfile
 from qc_tool.common import FAILED_ITEMS_LIMIT
 
 
-def dir_recursive_search(in_dir, regexp=".*", target="file", deep=9999, full_path=True):
-    results = []
-    level = 0
-    for root, dirs, files in os.walk(in_dir):
-        res = [f for f in os.listdir(root) if re.search(r"{:s}".format(regexp), f)]
-        for f in res:
-            path = os.path.join(root, f)
-            if ((target == "file" and os.path.isfile(path)) or (target == "dir" and os.path.isdir(path))) and (level <= deep):
-                if full_path:
-                    results.append(path)
-                else:
-                    results.append(f)
-        level += 1
-    return results
-
 def do_layers(params):
     return [params["layer_defs"][layer_alias] for layer_alias in params["layers"]]
 
