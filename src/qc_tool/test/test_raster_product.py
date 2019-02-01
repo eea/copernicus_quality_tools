162259474eba99d40669c1de3646e83f166da086
jirikadlec2 <jirikadlec2@gmail.com>
2019-02-01 20:47:04 +0100

    r12 metadata check in test_raster_product is intentionally skipped.
    r12 is already tested in test_raster_check.
    in test_raster_product it is skipped to reduce the number of requests sent to
    INSPIRE validator server while running tests.

--- src/qc_tool/test/test_raster_product.py
+++ src/qc_tool/test/test_raster_product.py
@@ -12,7 +12,7 @@ class Test_Raster(ProductTestCase):
         Helper function to display messages of aborted, failed or skipped checks in
         case of test failure.
         """
-        checks_not_ok = [check for check in job_status["checks"] if check["status"] != "ok"]
+        checks_not_ok = [check for check in job_status["checks"] if check["status"] not in ("ok", "skipped")]
         msg = ""
         for check in checks_not_ok:
             check_ident = check["check_ident"]
@@ -29,175 +29,141 @@ class Test_Raster(ProductTestCase):
         self.username = "test_username"
 
         # these optional checks are present in all tested HRL raster product definitions.
-        self.check_idents = ["r3", "r4", "r5", "r6", "r7", "r8", "r9", "r10", "r12", "r14", "r15"]
+        # NOTE: r12 INSPIRE metadata check is skipped on purpose. It is already tested in test_raster_check.
+        self.check_idents = ["r3", "r4", "r5", "r6", "r7", "r8", "r9", "r10", "r14", "r15"]
+
+        # all checks are expected to finish with ok status by default.
+        self.expected_check_statuses = {"r_unzip": "ok",
+                                        "r1": "ok",
+                                        "r2": "ok",
+                                        "r3": "ok",
+                                        "r4": "ok",
+                                        "r5": "ok",
+                                        "r6": "ok",
+                                        "r7": "ok",
+                                        "r8": "ok",
+                                        "r9": "ok",
+                                        "r10": "ok",
+                                        "r12": "skipped", # INSPIRE metadata check is skipped in product tests.
+                                        "r14": "ok",
+                                        "r15": "ok"}
 
     # High resolution forest type (FTY) - 20m
     def test_fty_020m(self):
         product_ident = "fty_020m"
         filepath = self.raster_data_dir.joinpath(product_ident, "FTY_2015_020m_mt_03035_d04_clip.zip")
-
         # fty_020m has extra check r11 (raster MMU)
         self.check_idents.append("r11")
-
-        expected_check_statuses = {"r_unzip": "ok",
-                                   "r1": "ok",
-                                   "r2": "ok",
-                                   "r3": "ok",
-                                   "r4": "ok",
-                                   "r5": "ok",
-                                   "r6": "ok",
-                                   "r7": "ok",
-                                   "r8": "ok",
-                                   "r9": "ok",
-                                   "r10": "ok",
-                                   "r11": "ok",
-                                   "r12": "ok",
-                                   "r14": "ok",
-                                   "r15": "ok"}
-
+        self.expected_check_statuses["r11"] = "ok"
         job_status = dispatch(self.job_uuid, self.username, filepath, product_ident, self.check_idents)
         check_statuses = dict((check_status["check_ident"], check_status["status"])
                               for check_status in job_status["checks"])
-        self.assertDictEqual(expected_check_statuses, check_statuses)
+        self.assertDictEqual(self.expected_check_statuses, check_statuses, self.show_messages(job_status))
 
     # High resolution forest type (FTY) - 100m
     def test_fty_100m(self):
         product_ident = "fty_100m"
         filepath = self.raster_data_dir.joinpath(product_ident, "fty_2015_100m_mt_03035_d02_clip.zip")
         job_status = dispatch(self.job_uuid, self.username, filepath, product_ident, self.check_idents)
-        self.assertTrue(all([check["status"] == "ok" for check in job_status["checks"]]),
-                        self.show_messages(job_status))
+        check_statuses = dict((check_status["check_ident"], check_status["status"])
+                              for check_status in job_status["checks"])
+        self.assertDictEqual(self.expected_check_statuses, check_statuses, self.show_messages(job_status))
 
     # High resolution grassland (GRA) - 20m
     def test_gra_020m(self):
         product_ident = "gra_020m"
         filepath = self.raster_data_dir.joinpath(product_ident, "GRA_2015_020m_mt_03035_V1_clip.zip")
-
         # gra_020m has extra check r11 (raster MMU)
         self.check_idents.append("r11")
-
-        expected_check_statuses = {"r_unzip": "ok",
-                                   "r1": "ok",
-                                   "r2": "ok",
-                                   "r3": "ok",
-                                   "r4": "ok",
-                                   "r5": "ok",
-                                   "r6": "ok",
-                                   "r7": "ok",
-                                   "r8": "ok",
-                                   "r9": "ok",
-                                   "r10": "ok",
-                                   "r11": "ok",
-                                   "r12": "ok",
-                                   "r14": "ok",
-                                   "r15": "ok"}
-
+        self.expected_check_statuses["r11"] = "ok"
+        # gra_020m has mismatching attributes
+        self.expected_check_statuses["r3"] = "failed"
         job_status = dispatch(self.job_uuid, self.username, filepath, product_ident, self.check_idents)
         check_statuses = dict((check_status["check_ident"], check_status["status"])
                               for check_status in job_status["checks"])
-        self.assertDictEqual(expected_check_statuses, check_statuses)
+        self.assertDictEqual(self.expected_check_statuses, check_statuses, self.show_messages(job_status))
 
     # High resolution grassland (GRA) - 100m
     def test_gra_100m(self):
         product_ident = "gra_100m"
         filepath = self.raster_data_dir.joinpath(product_ident, "GRA_2015_100m_mt_03035_V1_clip.zip")
         job_status = dispatch(self.job_uuid, self.username, filepath, product_ident, self.check_idents)
-        self.assertTrue(all([check["status"] == "ok" for check in job_status["checks"]]),
-                        self.show_messages(job_status))
+        check_statuses = dict((check_status["check_ident"], check_status["status"])
+                              for check_status in job_status["checks"])
+        self.assertDictEqual(self.expected_check_statuses, check_statuses, self.show_messages(job_status))
 
     # High resolution imperviousness change (IMC) - 20m
     def test_imc_020m(self):
         product_ident = "imc_020m"
         filepath = self.raster_data_dir.joinpath(product_ident, "IMC_1215_020m_mt_03035_d02_clip.zip")
         job_status = dispatch(self.job_uuid, self.username, filepath, product_ident, self.check_idents)
-        self.assertTrue(all([check["status"] == "ok" for check in job_status["checks"]]),
-                        self.show_messages(job_status))
+        check_statuses = dict((check_status["check_ident"], check_status["status"])
+                              for check_status in job_status["checks"])
+        self.assertDictEqual(self.expected_check_statuses, check_statuses, self.show_messages(job_status))
 
     # High resolution imperviousness change (IMC) - 100m
     def test_imc_100m(self):
         product_ident = "imc_100m"
         filepath = self.raster_data_dir.joinpath(product_ident, "IMC_1215_100m_mt_03035_d02_clip.zip")
         job_status = dispatch(self.job_uuid, self.username, filepath, product_ident, self.check_idents)
-        self.assertTrue(all([check["status"] == "ok" for check in job_status["checks"]]),
-                        self.show_messages(job_status))
+        check_statuses = dict((check_status["check_ident"], check_status["status"])
+                              for check_status in job_status["checks"])
+        self.assertDictEqual(self.expected_check_statuses, check_statuses, self.show_messages(job_status))
 
     # High resolution imperviousness density (IMD) - 20m
     def test_imd_020m(self):
         product_ident = "imd_020m"
         filepath = self.raster_data_dir.joinpath(product_ident, "IMD_2015_020m_mt_03035_d04_clip.zip")
         job_status = dispatch(self.job_uuid, self.username, filepath, product_ident, self.check_idents)
-        self.assertTrue(all([check["status"] == "ok" for check in job_status["checks"]]),
-                        self.show_messages(job_status))
+        check_statuses = dict((check_status["check_ident"], check_status["status"])
+                              for check_status in job_status["checks"])
+        self.assertDictEqual(self.expected_check_statuses, check_statuses, self.show_messages(job_status))
 
     # High resolution imperviousness density (IMD) - 100m
     def test_imd_100m(self):
         product_ident = "imd_100m"
         filepath = self.raster_data_dir.joinpath(product_ident, "IMD_2015_100m_mt_03035_d02_clip.zip")
         job_status = dispatch(self.job_uuid, self.username, filepath, product_ident, self.check_idents)
-        self.assertTrue(all([check["status"] == "ok" for check in job_status["checks"]]),
-                        self.show_messages(job_status))
+        check_statuses = dict((check_status["check_ident"], check_status["status"])
+                              for check_status in job_status["checks"])
+        self.assertDictEqual(self.expected_check_statuses, check_statuses, self.show_messages(job_status))
 
     # High resolution tree cover density (TCD) - 20m
     def test_tcd_020m(self):
         product_ident = "tcd_020m"
         filepath = self.raster_data_dir.joinpath(product_ident, "TCD_2015_020m_mt_03035_d04_clip.zip")
         job_status = dispatch(self.job_uuid, self.username, filepath, product_ident, self.check_idents)
-        self.assertTrue(all([check["status"] == "ok" for check in job_status["checks"]]),
-                        self.show_messages(job_status))
+        check_statuses = dict((check_status["check_ident"], check_status["status"])
+                              for check_status in job_status["checks"])
+        self.assertDictEqual(self.expected_check_statuses, check_statuses, self.show_messages(job_status))
 
     # High resolution tree cover density (TCD) - 100m
     def test_tcd_100m(self):
         product_ident = "tcd_100m"
         filepath = self.raster_data_dir.joinpath(product_ident, "TCD_2015_100m_mt_03035_d03_clip.zip")
         job_status = dispatch(self.job_uuid, self.username, filepath, product_ident, self.check_idents)
-        self.assertTrue(all([check["status"] == "ok" for check in job_status["checks"]]),
-                        self.show_messages(job_status))
+        check_statuses = dict((check_status["check_ident"], check_status["status"])
+                              for check_status in job_status["checks"])
+        self.assertDictEqual(self.expected_check_statuses, check_statuses, self.show_messages(job_status))
 
     # High resolution water and wetness (WAW) - 20m
     def test_waw_020m(self):
         product_ident = "waw_020m"
         filepath = self.raster_data_dir.joinpath(product_ident, "WAW_2015_020m_mt_03035_d06_clip.zip")
-        expected_check_statuses = {"r_unzip": "ok",
-                                   "r1": "ok",
-                                   "r2": "ok",
-                                   "r3": "ok",
-                                   "r4": "ok",
-                                   "r5": "ok",
-                                   "r6": "ok",
-                                   "r7": "ok",
-                                   "r8": "ok",
-                                   "r9": "ok",
-                                   "r10": "cancelled", #FIXME r10 should be ok. Provide a special raster mask for waw_020m.
-                                   "r12": "ok",
-                                   "r14": "ok",
-                                   "r15": "ok"}
-
+        # completeness check is cancelled due to unavailable raster mask.
+        self.expected_check_statuses["r10"] = "cancelled"
         job_status = dispatch(self.job_uuid, self.username, filepath, product_ident, self.check_idents)
         check_statuses = dict((check_status["check_ident"], check_status["status"])
                               for check_status in job_status["checks"])
-        self.assertDictEqual(expected_check_statuses, check_statuses)
+        self.assertDictEqual(self.expected_check_statuses, check_statuses)
 
     # High resolution water and wetness (WAW) - 100m
     def test_waw_100m(self):
         product_ident = "waw_100m"
         filepath = self.raster_data_dir.joinpath(product_ident, "WAW_2015_100m_mt_03035_d02_clip.zip")
-        expected_check_statuses = {"r_unzip": "ok",
-                                   "r1": "ok",
-                                   "r2": "ok",
-                                   "r3": "ok",
-                                   "r4": "ok",
-                                   "r5": "ok",
-                                   "r6": "ok",
-                                   "r7": "ok",
-                                   "r8": "ok",
-                                   "r9": "ok",
-                                   "r10": "cancelled",
-                                   # FIXME r10 should be ok. Provide a special raster mask for waw_100m.
-                                   "r12": "ok",
-                                   "r14": "ok",
-                                   "r15": "ok"}
-
+        # completeness check is cancelled due to unavailable raster mask.
+        self.expected_check_statuses["r10"] = "cancelled"
         job_status = dispatch(self.job_uuid, self.username, filepath, product_ident, self.check_idents)
         check_statuses = dict((check_status["check_ident"], check_status["status"])
                               for check_status in job_status["checks"])
-        self.assertDictEqual(expected_check_statuses, check_statuses)
+        self.assertDictEqual(self.expected_check_statuses, check_statuses)
