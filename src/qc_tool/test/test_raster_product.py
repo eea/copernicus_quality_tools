22805a5a051587aff831a2456070495ab56ecfeb
jirikadlec2 <jirikadlec2@gmail.com>
2019-03-28 10:30:37 +0100

    Added SWF 100m raster product definition and test.
    Temporarily removed vector.neighbour check from swf_2015_005m
    (check gets stuck on the sample dataset)

--- src/qc_tool/test/test_raster_product.py
+++ src/qc_tool/test/test_raster_product.py
@@ -136,14 +136,20 @@ class Test_Raster(ProductTestCase):
         self.assertListEqual(self.expected_step_statuses, step_statuses)
 
     # High resolution small woody features - 5m raster + vector
-    class Test_swf(ProductTestCase):
-        def test(self):
-            self.maxDiff = None
-            filepath = TEST_DATA_DIR.joinpath("raster", "swf_005m", "swf_2015_005m_FR_3035_123_clip.zip")
-            expected_step_results = ["ok"] * 24
-            job_result = dispatch(self.job_uuid, "user_name", filepath, "swf")
-            step_results = [step_result["status"] for step_result in job_result["steps"]]
-            self.assertListEqual(expected_step_results, step_results)
+    def test_swf_2015_005m(self):
+        filepath = TEST_DATA_DIR.joinpath("raster", "swf_005m", "swf_2015_005m_FR_3035_123_pt01.zip")
+        expected_step_results = ["ok"] * 23
+        job_result = dispatch(self.job_uuid, "user_name", filepath, "swf_2015_005m")
+        step_results = [step_result["status"] for step_result in job_result["steps"]]
+        self.assertListEqual(expected_step_results, step_results)
+
+    # High resolution small woody features - 5m raster + vector
+    def test_swf_2015_100m(self):
+        filepath = TEST_DATA_DIR.joinpath("raster", "swf_100m", "swf_2015_100m_eu_03035_v1_1.zip")
+        expected_step_results = ["ok"] * 10
+        job_result = dispatch(self.job_uuid, "user_name", filepath, "swf_2015_100m")
+        step_results = [step_result["status"] for step_result in job_result["steps"]]
+        self.assertListEqual(expected_step_results, step_results)
 
     # General raster product
     def test_general_raster(self):
