2c6ec06b34cfa411a464971fe554f97ce5124cc3
jirikadlec2 <jirikadlec2@gmail.com>
2019-04-04 12:10:24 +0200

    Added import to test helper.

--- src/qc_tool/test/helper.py
+++ src/qc_tool/test/helper.py
@@ -7,6 +7,7 @@ from uuid import uuid4
 
 from qc_tool.common import CONFIG
 from qc_tool.common import TEST_DATA_DIR
+from qc_tool.common import create_job_dir
 from qc_tool.worker.dispatch import CheckStatus
 from qc_tool.worker.manager import create_connection_manager
 from qc_tool.worker.manager import create_jobdir_manager
@@ -16,7 +17,7 @@ class ProductTestCase(TestCase):
     def setUp(self):
         super().setUp()
         self.job_uuid = str(uuid4())
-        create_job_dir(job_uuid)
+        create_job_dir(self.job_uuid)
 
         # Set up boundary dir to testing sources.
         CONFIG["boundary_dir"] = TEST_DATA_DIR.joinpath("boundaries")
