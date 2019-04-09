55207d4fc0290d04057c0ba43a017f836a1fb198
jirikadlec2 <jirikadlec2@gmail.com>
2019-04-09 22:38:33 +0200

    Reorganized frontend docker file and set up autobuild hook.

---
+++ docker/run_frontend.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+cd /usr/local/src/copernicus_quality_tools/src/qc_tool/frontend
+
+# Create frontend database and tables if they do not exist.
+python3 -m qc_tool.frontend.manage migrate
+
+# Create default user accounts.
+python3 -m qc_tool.frontend.manage create_default_user --username admin --password admin --superuser
+python3 -m qc_tool.frontend.manage create_default_user --username guest --password guest
+python3 -m qc_tool.frontend.manage create_default_user --username guest2 --password guest2
+python3 -m qc_tool.frontend.manage create_default_user --username guest3 --password guest3
+
+# Start Django development server.
+python3 -m qc_tool.frontend.manage runserver 0.0.0.0:${FRONTEND_PORT:-8000}
