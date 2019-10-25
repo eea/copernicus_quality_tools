766265edf15b87a41127015c68192672954be859
jirikadlec2 <jirikadlec2@gmail.com>
2019-10-25 15:37:57 +0200

    Fixed incorrect description of qc_tool.vector.unzip check.
    import statement for qc_tool.vector.helper has been moved inside the run_check function because qc_tool.vector.helper imports the requests library which is not installed in the frontend.

--- src/qc_tool/vector/unzip.py
+++ src/qc_tool/vector/unzip.py
@@ -2,14 +2,12 @@
 # -*- coding: utf-8 -*-
 
 
-from qc_tool.vector.helper import do_unzip
-
-
 DESCRIPTION = "Delivery file can be unzipped."
 IS_SYSTEM = True
 
 
 def run_check(params, status):
+    from qc_tool.vector.helper import do_unzip
     # Vector layers are unzipped to the temporary directory v_unzip.d.
     do_unzip(params["filepath"], params["tmp_dir"].joinpath("v_unzip.d"), status)
 
