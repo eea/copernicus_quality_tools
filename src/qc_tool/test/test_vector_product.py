da49cdd4f5cf5720b57341c07b1a85ab32db77d2
jirikadlec2 <jirikadlec2@gmail.com>
2020-02-21 10:55:49 +0100

    Added product tests for coastal zones.

--- src/qc_tool/test/test_vector_product.py
+++ src/qc_tool/test/test_vector_product.py
@@ -120,3 +120,40 @@ class Test_ua2018_stl(ProductTestCase):
         step_results = [step_result["status"] for step_result in job_result["steps"]]
         self.maxDiff = None
         self.assertListEqual(expected_step_results, step_results)
+
+class Test_cz_2012(ProductTestCase):
+    def test_gpkg(self):
+        filepath = TEST_DATA_DIR.joinpath("vector", "cz", "gdb", "CZ_2012_DU001_3035_V1_0.gdb.zip")
+        expected_step_results = ["ok"] * 16
+        # vector.inspire check is skipped
+        expected_step_results[5] = "skipped"
+
+        job_result = dispatch(self.job_uuid, "user_name", filepath, "cz_2012", (6,))
+        step_results = [step_result["status"] for step_result in job_result["steps"]]
+        self.maxDiff = None
+        self.assertListEqual(expected_step_results, step_results)
+
+class Test_cz_2018(ProductTestCase):
+    def test_gpkg(self):
+        filepath = TEST_DATA_DIR.joinpath("vector", "cz", "gdb", "CZ_2018_DU001_3035_V1_0.gdb.zip")
+        expected_step_results = ["ok"] * 16
+        # vector.inspire check is skipped
+        expected_step_results[5] = "skipped"
+
+        job_result = dispatch(self.job_uuid, "user_name", filepath, "cz_2018", (6,))
+        step_results = [step_result["status"] for step_result in job_result["steps"]]
+        self.maxDiff = None
+        self.assertListEqual(expected_step_results, step_results)
+
+class Test_cz_change_2012_2018(ProductTestCase):
+    def test_gpkg(self):
+        filepath = TEST_DATA_DIR.joinpath("vector", "cz", "gdb", "CZ_change_2012_2018_DU001_3035_V1_0.gdb.zip")
+        expected_step_results = ["ok"] * 17
+        # vector.inspire check is skipped
+        expected_step_results[5] = "skipped"
+
+        job_result = dispatch(self.job_uuid, "user_name", filepath, "cz_change_2012_2018", (6,))
+        step_results = [step_result["status"] for step_result in job_result["steps"]]
+        self.maxDiff = None
+        self.assertListEqual(expected_step_results, step_results)
+
