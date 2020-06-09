1abc29be76c3d4c07a0b75ce491f6c940dcf95d2
jirikadlec2 <jirikadlec2@gmail.com>
2020-06-09 11:11:16 +0200

    vector.overlap check double-checks overlap candidates using original geometries.
    Due to a PostGIS issue, coordinates of partitioned polygons are shifted in rare cases.
    This might produce false overlap positives.
    To overcome the issue, vector.overlap checks looks at all entries in neighbour table with dim > 1
    and re-checks the dimension of the intersections of overlap candidate pairs using their original geometries.
    
    A new overlap_detail attachment has also been added to allow the user to inspect exact locations of overlap intersection geometries.

--- src/qc_tool/vector/overlap.py
+++ src/qc_tool/vector/overlap.py
@@ -28,18 +28,38 @@ def run_check(params, status):
 
         # Create table of error items.
         sql_params = {"fid_name": layer_def["pg_fid_name"],
+                      "feature_table": layer_def["pg_layer_name"],
                       "neighbour_table": neighbour_table.neighbour_table_name,
                       "error_table": "s{:02d}_{:s}_error".format(params["step_nr"], layer_def["pg_layer_name"])}
         sql = ("CREATE TABLE {error_table} AS\n"
-               "SELECT DISTINCT fida AS {fid_name}\n"
-               "FROM {neighbour_table}\n"
-               "WHERE dim >= 2;")
+               "SELECT DISTINCT a.fid AS {fid_name}\n"
+               "FROM\n"
+               " (SELECT fid, geom FROM {feature_table}\n"
+               "   WHERE fid in (SELECT fida from {neighbour_table} WHERE dim > 1)) AS a,\n"
+               " (SELECT fid, geom FROM {feature_table}\n"
+               "   WHERE fid in (SELECT fidb from {neighbour_table} WHERE dim > 1)) AS b\n"
+               "WHERE a.fid <> b.fid AND ST_Dimension(ST_Intersection(a.geom, b.geom)) >= 2\n")
         sql = sql.format(**sql_params)
         cursor.execute(sql)
 
         # Report error items.
         items_message = get_failed_items_message(cursor, sql_params["error_table"], layer_def["pg_fid_name"])
         if items_message is not None:
+
+            # If there are error items are found, then add a full table with intersection areas.
+            sql = ("CREATE TABLE {overlap_detail_table} AS\n"
+               "SELECT DISTINCT a.fid AS fida, b.fid as fidb, polygon_dump(ST_Intersection(a.geom, b.geom)) as geom\n"
+               "FROM\n"
+               " (SELECT fid, geom FROM {feature_table}\n"
+               "   WHERE fid in (SELECT fida from {neighbour_table} WHERE dim > 1)) AS a,\n"
+               " (SELECT fid, geom FROM {feature_table} "
+               "   WHERE fid in (SELECT fidb from {neighbour_table} WHERE dim > 1)) AS b\n"
+               "WHERE a.fid > b.fid AND ST_Dimension(ST_Intersection(a.geom, b.geom)) >= 2\n")
+            sql_params["overlap_detail_table"] = "s{:02d}_{:s}_detail".format(params["step_nr"], layer_def["pg_layer_name"])
+            sql = sql.format(**sql_params)
+            cursor.execute(sql)
+            status.add_full_table(sql_params["overlap_detail_table"])
+
             status.failed("Layer {:s} has overlapping pairs in features with {:s}: {:s}."
                           .format(layer_def["pg_layer_name"], layer_def["fid_display_name"], items_message))
             status.add_error_table(sql_params["error_table"], layer_def["pg_layer_name"], layer_def["pg_fid_name"])
