c804a2d03aa4c8a98134f790bff0f81ffb883e5a
jirikadlec2 <jirikadlec2@gmail.com>
2020-12-17 16:13:28 +0100

    raster.origin check - added a customizable grid_size parameter.
    The grid_size parameter specifies the size of the grid (in units of the raster crs) that the raster origin should be exactly aligned to.
    The default value of grid_size is 1000 metres (LEAC 1km grid).
    If the coordinates of the raster origin  should be integers, then grid_size parameter should be set to 1 metre.

--- src/qc_tool/raster/origin.py
+++ src/qc_tool/raster/origin.py
@@ -10,6 +10,8 @@ def run_check(params, status):
     import osgeo.gdal as gdal
     from qc_tool.raster.helper import do_raster_layers
 
+    grid_size = params.get("grid_size", 1000)
+
     for layer_def in do_raster_layers(params):
         ds = gdal.Open(str(layer_def["src_filepath"]))
 
@@ -20,12 +22,12 @@ def run_check(params, status):
         pixelsizex = gt[1]
         pixelsizey = gt[5]
 
-        if ulx % pixelsizex != 0 or uly % pixelsizey != 0:
-            status.failed("The upper-left X, Y coordinates of layer {:s} are not divisible by pixel-size with no remainder."
-                          .format(layer_def["src_layer_name"]))
+        if "grid_size" in params and params["grid_size"] >= pixelsizex and params["grid_size"] >= pixelsizey:
+            if ulx % pixelsizex != 0 or uly % pixelsizey != 0:
+                status.failed("The upper-left X, Y coordinates of layer {:s} are not divisible by pixel-size with no remainder."
+                              .format(layer_def["src_layer_name"]))
 
-            # Pan-European layers must fit to the LEAC 1 km grid
-            if "_eu_" in layer_def["src_layer_name"]:
-                if ulx % 1000 != 0 or uly % 1000 != 0:
-                    status.failed("The raster origin of layer {:s} does not fit to the LEAC 1 km grid."
-                                  .format(layer_def["src_layer_name"]))
+        # Layers must fit to the specified grid
+        if ulx % grid_size != 0 or uly % grid_size != 0:
+            status.failed("The raster origin of layer {:s} does not align to the LEAC {} metre grid."
+                          .format(layer_def["src_layer_name"], grid_size))
